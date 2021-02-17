904f6c7a4c4c7fc23f0d94edbd06a92f5dff9d0b
wdzfxs <voylokovdima@mail.ru>
2021-02-17 22:08:32 +0300

    Added currency feign client & basic properties

--- src/main/java/com/wdzfxs/currencyexchangerate/CurrencyExchangeRateApplication.java
+++ src/main/java/com/wdzfxs/currencyexchangerate/CurrencyExchangeRateApplication.java
@@ -2,12 +2,14 @@ package com.wdzfxs.currencyexchangerate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyExchangeRateApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyExchangeRateApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/wdzfxs/currencyexchangerate/service/CurrencyFeignClient.java
@@ -0,0 +1,23 @@
+package com.wdzfxs.currencyexchangerate.service;
+
+import com.wdzfxs.currencyexchangerate.dto.Currency;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@FeignClient(name = "currency-client", url = "https://openexchangerates.org")
+public interface CurrencyFeignClient {
+
+    @GetMapping("/api/currencies.json?show_alternative=true&show_inactive=true")
+    ResponseEntity<Map<String, String>> allCurrencies();
+
+    @GetMapping("/api/latest.json?app_id=${open-exchange-rates.app-id}&base=${base-currency}&symbols={symbol}&prettyprint=false&show_alternative=true")
+    ResponseEntity<Currency> latestSymbolRate(@RequestParam("symbol") String symbol);
+
+    @GetMapping("/api/historical/{date}.json?app_id=${open-exchange-rates.app-id}&base=${base-currency}&show_alternative=true&prettyprint=false")
+    ResponseEntity<Currency> yesterdaySymbolRate(@PathVariable("date") String date, @RequestParam("symbol") String symbol);
+}
