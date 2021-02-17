782f5a431c57299666d6e05d3d54c0a312444ca0
wdzfxs <voylokovdima@mail.ru>
2021-02-17 14:31:22 +0300

    Created

---
+++ src/test/java/com/wdzfxs/currencyexchangerate/CurrencyExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdzfxs.currencyexchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
