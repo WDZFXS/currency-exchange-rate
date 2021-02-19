9fe4a7e52540db3e994e1cd7a97bce41f0dad08d
wdzfxs <voylokovdima@mail.ru>
2021-02-20 01:54:13 +0300

    Added types of external API exceptions

---
+++ src/main/java/com/wdzfxs/currencyexchangerate/client/CurrencyResponseException.java
@@ -0,0 +1,31 @@
+package com.wdzfxs.currencyexchangerate.client;
+
+/*
+List of potential API errors https://docs.openexchangerates.org/docs/errors
+ */
+public class CurrencyResponseException {
+
+    public static class NotFound extends RuntimeException {
+    }
+
+    public static class MissingAppId extends RuntimeException {
+        public MissingAppId(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidAppId extends RuntimeException {
+        public InvalidAppId(String message) {
+            super(message);
+        }
+    }
+
+    public static class NotAllowed extends RuntimeException {
+    }
+
+    public static class AccessRestricted extends RuntimeException {
+    }
+
+    public static class InvalidBase extends RuntimeException {
+    }
+}
---
+++ src/main/java/com/wdzfxs/currencyexchangerate/client/GiphyResponseException.java
@@ -0,0 +1,13 @@
+package com.wdzfxs.currencyexchangerate.client;
+
+/*
+List of potential API errors https://developers.giphy.com/docs/api/#response-codes
+ */
+public class GiphyResponseException {
+
+    public static class Unauthorized extends RuntimeException {
+    }
+
+    public static class TooManyRequests extends RuntimeException {
+    }
+}
